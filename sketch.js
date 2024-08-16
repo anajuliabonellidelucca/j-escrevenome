6a56225ac420c4020158299ef2e0c9fece7c30fc
Aninhaa :) <lucca.ana@escola.pr.gov.br>
2024-08-16 10:22:11 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,16 @@
 function setup() {
-    createCanvas(400, 400);
-    background("white")
-  }
+  createCanvas(800, 1200);
+  background("rgb(218,218,218)")
+}
+
+function draw() {
+  stroke("blue");
+  fill("#03A9F4");
   
-  function draw() {
-    stroke("blue");
-    fill("#03A9F4");
-    
-    
-    if (mouseIsPressed) {
-      rect(mouseX, mouseY, 20, 35);
-    }
+  
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
   }
+}
+
   
